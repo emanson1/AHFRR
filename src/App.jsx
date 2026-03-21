91f4b9ca1770c1cd00f5f00dc3560cf6cb09e862
emanson1 <edwardmaddenanson@gmail.com>
2026-03-21 19:07:27 -0400

    origin

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import '../src/App.css';
+import MainPage from './Pages/MainPage';
+import ModalRoot from './Shared/ModalRoot';
+import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import { hideModal } from './actions/bluevilleActions';
+import Navigation from './Pages/Navigation.jsx';
+import Home from './Pages/Home.jsx';
+import Services from './Pages/Services.jsx';
+import Gallery from './Pages/Gallery.jsx';
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+const useStyles = makeStyles((theme) => ({
+}))
+
+function App(props) {
+  const modalProps=props.modalProps;
+  const modalType=modalProps.modalType;
+  const open=modalProps.open;
+  const [openNav, setOpen] = useState(false);
+  const [rightOpen, setRightOpen] = useState(false);
+  const [leftOpen, setLeftOpen] = useState(false);
+  const [curWindow, setCurWindow] = useState(<Home />);
+  return (
+    <React.Fragment>
+      <Router>
+        <Route
+  exact path='/'
+  render={(props) => (
+    <MainPage  curWindow={curWindow} setCurWindow={setCurWindow} open={openNav} setOpen={setOpen} {...props}  Home={<Home />} Services={<Services />} Gallery={<Gallery />}/>
+  )}
+/>
+<Navigation open={openNav} setOpen={setOpen} curWindow={curWindow} setCurWindow={setCurWindow} Home={<Home />} Services={<Services />}  Gallery={<Gallery />}/>
+      </Router>
+    
+       {modalType!==undefined && modalType!==null && <ModalRoot handleClose={hideModal} open={open}/> }
+       
+       </React.Fragment>
+ 
+       )
+}
+const mapStateToProps = (state) => ({
+  modalProps: state.blueville.modalProps,
+  modalType: state.blueville.modalTYpe
+});
+const mapDispatchToProps = (dispatch) => ({
+  hideModal: () => dispatch(hideModal()),
+  
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createHashHistory } from 'history';
+import { ConnectedRouter } from 'connected-react-router';
+import { createRoot } from 'react-dom/client';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+
+const base = document.querySelector('base');
+const baseUrl = base && base.href || '';
+const history = createHashHistory({basename: baseUrl});
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
+const rootElement = document.getElementById('root');
+
+const configuration = {
+  applicationName: 'AHFRR - 1.0.0',
+  applicationAcronym:'AHFRR',
+  folder:''
+};
+const container = document.getElementById('root'); // Or your actual DOM node variable
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
+registerServiceWorker();
